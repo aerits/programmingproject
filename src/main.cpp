a78f79d522832e72b88ad40596162f118f279933
aerits <stev_nm@protonmail.com>
2022-04-14 11:33:04 -0600

    fixed typo

--- src/main.cpp
+++ src/main.cpp
@@ -7,5 +7,5 @@ int main() {
 };
 
 void startScreen() {
-	cout << "  a b c d e f g h\n|-----------------| \n|-----------------| \n|                 | 2\n|  welcome to     | 3\n|   c++thello     | 4\n|                 | 5\n|    1. start     | 6\n|    1. start     | 6\n|    1. start     | 6\n|    1. start     | 6\n"
+	cout << "  a b c d e f g h\n|-----------------| \n|-----------------| \n|                 | 2\n|  welcome to     | 3\n|   c++thello     | 4\n|                 | 5\n|    1. start     | 6\n|    1. start     | 6\n|    1. start     | 6\n|    1. start     | 6\n";
 }
