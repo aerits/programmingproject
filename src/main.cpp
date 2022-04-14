743f80455bca7b51919c744c6e65e1d71b767e85
aerits <stev_nm@protonmail.com>
2022-04-14 11:38:33 -0600

    fixed start screen

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 void startScreen() {
-	cout << "  a b c d e f g h\n|-----------------| \n|-----------------| \n|                 | 2\n|  welcome to     | 3\n|   c++thello     | 4\n|                 | 5\n|    1. start     | 6\n|    1. start     | 6\n|    1. start     | 6\n|    1. start     | 6\n";
+	cout << "  a b c d e f g h\n|-----------------| \n|                 | 1\n|                 | 2\n|  welcome to     | 3\n|   c++thello     | 4\n|                 | 5\n|    1. start     | 6\n|    2. quit      | 7\n|                 | 8\n|-----------------|\n";
 }
 
 int main() {
