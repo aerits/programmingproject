e953b03589519e34f3441ff79009b16318eaa73d
aerits <stev_nm@protonmail.com>
2022-04-14 11:43:35 -0600

    added end screen

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,12 @@ void startScreen() {
 	cout << "  a b c d e f g h\n|-----------------| \n|                 | 1\n|                 | 2\n|  welcome to     | 3\n|   c++thello     | 4\n|                 | 5\n|    1. start     | 6\n|    2. quit      | 7\n|                 | 8\n|-----------------|\n";
 }
 
+void endScreen() {
+	cout << "  a b c d e f g h\n|-----------------| \n|                 | 1\n|                 | 2\n|                 | 3\n|     game end    | 4\n|                 | 5\n|    1. play again| 6\n|    2. quit      | 7\n|                 | 8\n|-----------------|\n";
+}
+
 int main() {
 	startScreen();
+	endScreen();
 	return 0;
 };
