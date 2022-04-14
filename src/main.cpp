a5bbbaaf1d3eb30d08f8c54dfebf3fdabe110880
aerits <stev_nm@protonmail.com>
2022-04-14 11:32:27 -0600

    added start screen

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,10 @@
 using namespace std;
 
 int main() {
+	startScreen();
 	return 0;
 };
 
 void startScreen() {
-	
+	cout << "  a b c d e f g h\n|-----------------| \n|-----------------| \n|                 | 2\n|  welcome to     | 3\n|   c++thello     | 4\n|                 | 5\n|    1. start     | 6\n|    1. start     | 6\n|    1. start     | 6\n|    1. start     | 6\n"
 }
