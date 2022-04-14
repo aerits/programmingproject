9229f437ef656cfe208d3527b1f9debc0fcbb83a
aerits <stev_nm@protonmail.com>
2022-04-14 12:37:24 -0600

    files work together now

---
+++ src/ai.h
@@ -0,0 +1,14 @@
+#ifndef AI_H 
+#define AI_H 
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class ai {
+	public:
+		void action(void);
+		void checkPlayableSpaces(void);
+};
+
+#endif // CPU_H
---
+++ src/disc.h
@@ -0,0 +1,14 @@
+#ifndef DISC_H
+#define DISC_H
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class disc {
+	public:
+		int x, y;
+		bool black;
+};
+
+#endif // DISC_H
