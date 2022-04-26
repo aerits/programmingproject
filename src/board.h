5ebc66917ee1eac57dffcc098ef49e737ab76e4c
aerits <stev_nm@protonmail.com>
2022-04-26 11:50:28 -0600

    lfdsalj

--- src/board.h
+++ src/board.h
@@ -7,11 +7,12 @@ using namespace std;
 
 class board {
 	public:
-		string boardSpaces[64];
+		string BoardSpaces[63];
 		int blackPuckCount;
 		int whitePuckCount;
 		void drawBoard(void);
 		void updateBoard(int x, int y, string mark);
+		void setBlackPuckCount(int num);
 };
 
 #endif // BOARD_H
