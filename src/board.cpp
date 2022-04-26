3c6facdd020992b9d2ba08271a0d09594c824d44
aerits <stev_nm@protonmail.com>
2022-04-26 12:11:05 -0600

    cool functionsgit commit board.h -m faklsjf

--- src/board.cpp
+++ src/board.cpp
@@ -4,10 +4,18 @@
 
 using namespace std;
 
-void drawBoard(void) {
-	cout << "  a b c d e f g h\n|-----------------|";
-	cout << "| ";
-	for(int i = 0;i>10;i++){
-		cout << " ";
+void board::updateBoard(int x, int y, string mark) {
+        BoardSpaces[(y*8)+x-1] = mark;
+}
+
+void board::drawBoard(void) {
+        cout << "  a b c d e f g h\n|-----------------|" << endl;
+	for(int a = 0;a<8;a++){
+	 	cout << "|";
+        	for(int i = 0;i<8;i++){
+                	cout << " " << BoardSpaces[(a*8)+i];
+        	}
+		cout << " | " << a+1 << endl;
 	}
 }
+
