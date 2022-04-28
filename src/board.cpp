733f4c9faaa3824226420faf64eb95d97c43fe6d
aerits <stev_nm@protonmail.com>
2022-04-28 12:24:56 -0600

    added the guts of the disc class

--- src/board.cpp
+++ src/board.cpp
@@ -1,11 +1,12 @@
 #include "board.h"
+#include "disc.h"
 #include <iostream>
 #include <string>
 
 using namespace std;
 
-void board::updateBoard(int x, int y, string mark) {
-        BoardSpaces[((y-1)*8)+x] = mark;
+void board::updateBoard(int x, int y, string mark, bool on){
+        BoardSpaces[((y-1)*8)+x].setColor(mark, on); 
 }
 
 void board::drawBoard(void) {
@@ -13,7 +14,7 @@ void board::drawBoard(void) {
 	for(int a = 0;a<8;a++){
 	 	cout << "|";
         	for(int i = 0;i<8;i++){
-                	cout << " " << BoardSpaces[(a*8)+i+1];
+                	cout << " " << BoardSpaces[(a*8)+i+1].checkColor();
         	}
 		cout << " | " << a+1 << endl;
 	}
@@ -21,17 +22,17 @@ void board::drawBoard(void) {
 }
 
 string board::checkSpace(int x, int y) {
-	return BoardSpaces[((y-1)*8)+x];
+	return BoardSpaces[((y-1)*8)+x].checkColor();
 }
 
-void board::setupBoard() {
+void board::setupBoard(){
 	for(int i = 1;i<9;i++){
                 for(int y = 1;y<9;y++){
-                        updateBoard(y, i, " ");
-                }
+                	BoardSpaces[((i-1)*8)+y].spawn(y, i, false, " ");
+		}
         }
-	updateBoard(4, 4, "O");
-	updateBoard(5, 5, "O");
-	updateBoard(4, 5, "0");
-	updateBoard(5, 4, "0");
+	updateBoard(4, 4, "O", true);
+	updateBoard(5, 5, "O", true);
+	updateBoard(4, 5, "0", true);
+	updateBoard(5, 4, "0", true);
 }
